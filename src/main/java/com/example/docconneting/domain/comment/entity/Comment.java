1d826d1bc7b42987af403cd7b30d7f6bb83bf441
tgg <qazplmtfgcv123@gmail.com>
2025-04-03 18:41:08 +0900

    feat: Comment 엔티티 작성 (#1)

---
+++ src/main/java/com/example/docconneting/domain/comment/entity/Comment.java
@@ -0,0 +1,28 @@
+package com.example.docconneting.domain.comment.entity;
+
+import com.example.docconneting.common.base.BaseEntity;
+import com.example.docconneting.domain.post.entity.Post;
+import com.example.docconneting.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    private String contents;
+}
