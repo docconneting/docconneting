0d29dafa6bd40e99a4bc4cd339b2d902a2649f52
tgg <qazplmtfgcv123@gmail.com>
2025-04-03 18:30:52 +0900

    feat: User 엔티티, UserRole Enum 작성 (#1)

---
+++ src/main/java/com/example/docconneting/domain/user/entity/User.java
@@ -0,0 +1,53 @@
+package com.example.docconneting.domain.user.entity;
+
+import com.example.docconneting.common.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String username;
+
+    private Integer point;
+
+    private String major;
+
+    private String image;
+
+    private LocalDateTime start_time;
+
+    private LocalDateTime end_time;
+
+    private UserRole userRole;
+
+    private Boolean isDeleted;
+
+    @Builder
+    public User(String email, String password, String username, Integer point, String major, String image, LocalDateTime start_time, LocalDateTime end_time, Boolean isDeleted, UserRole userRole) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.point = point;
+        this.major = major;
+        this.image = image;
+        this.start_time = start_time;
+        this.end_time = end_time;
+        this.isDeleted = isDeleted;
+        this.userRole = userRole;
+    }
+}
---
+++ src/main/java/com/example/docconneting/domain/user/entity/UserRole.java
@@ -0,0 +1,4 @@
+package com.example.docconneting.domain.user.entity;
+
+public enum UserRole {
+}
