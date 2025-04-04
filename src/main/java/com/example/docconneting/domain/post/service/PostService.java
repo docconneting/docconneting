7b5eaf2c74c2390fe1d008efeda19ce585567c8c
tgg <qazplmtfgcv123@gmail.com>
2025-04-04 16:42:26 +0900

    Feat: 게시물 삭제 구현 (#4)

--- src/main/java/com/example/docconneting/domain/post/service/PostService.java
+++ src/main/java/com/example/docconneting/domain/post/service/PostService.java
@@ -8,6 +8,7 @@ import com.example.docconneting.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,6 +18,7 @@ public class PostService {
     private final PostRepository postRepository;
 
     // 게시물 단건 조회
+    @Transactional(readOnly = true)
     public PostSingleResponse findPostById(Long postId){
         Post findPost = postRepository.findById(postId).orElseThrow(() -> new ClientException(ErrorCode.NOT_FOUND_POST));
 
@@ -35,5 +37,12 @@ public class PostService {
                 .createdAt(findPost.getCreatedAt())
                 .modifiedAt(findPost.getModifiedAt())
                 .build();
+    }
+
+    // 게시물 삭제
+    @Transactional
+    public void deletePostById(Long postId){
+        Post findPost = postRepository.findById(postId).orElseThrow(() -> new ClientException(ErrorCode.NOT_FOUND_POST));
+        findPost.delete();
     }
 }
