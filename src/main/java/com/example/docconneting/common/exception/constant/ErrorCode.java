1a4b1c0bdfb16456b22bc386a221fa39e5eeb191
kyn1013 <yn112511@naver.com>
2025-04-04 20:54:55 +0900

    Feat : 의사 검색 다건 조회 기능 추가 (#5)

--- src/main/java/com/example/docconneting/common/exception/constant/ErrorCode.java
+++ src/main/java/com/example/docconneting/common/exception/constant/ErrorCode.java
@@ -14,7 +14,8 @@ public enum ErrorCode {
 
     // 의사 조회 에러코드
     //404
-    DOCTOR_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 의사입니다.");
+    DOCTOR_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 의사입니다."),
+    MAJOR_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 전공입니다.");
 
     private final HttpStatus status;
     private final String message;
--- src/main/java/com/example/docconneting/domain/doctor/service/DoctorService.java
+++ src/main/java/com/example/docconneting/domain/doctor/service/DoctorService.java
@@ -2,11 +2,18 @@ package com.example.docconneting.domain.doctor.service;
 
 import com.example.docconneting.common.exception.constant.ErrorCode;
 import com.example.docconneting.common.exception.object.ClientException;
+import com.example.docconneting.common.response.PageInfo;
+import com.example.docconneting.common.response.PageResult;
 import com.example.docconneting.domain.doctor.dto.DoctorResponse;
 import com.example.docconneting.domain.user.entity.User;
 import com.example.docconneting.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -28,5 +35,21 @@ public class DoctorService {
                 .build();
 
         return response;
+    }
+
+    // 의사 다건 조회 검색
+    public PageResult<User> findDoctors(int page, int size, String category, String name) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        Page<User> result = userRepository.findDoctors(pageable, category, name);
+        List<User> users = result.getContent().stream().toList();
+
+        PageInfo pageInfo = PageInfo.builder()
+                .pageNum(page)
+                .pageSize(size)
+                .totalElement(result.getTotalElements())
+                .totalPage(result.getTotalPages())
+                .build();
+
+        return new PageResult<>(users, pageInfo);
     }
 }
