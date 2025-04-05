5cc6deef0cf5661e52fed4fc29c88b49555e4aba
tgg <qazplmtfgcv123@gmail.com>
2025-04-05 16:21:54 +0900

    Fix: PostService findPostById() N+1 문제 해결 (#4)

--- src/main/java/com/example/docconneting/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/docconneting/domain/post/repository/PostRepository.java
@@ -2,6 +2,12 @@ package com.example.docconneting.domain.post.repository;
 
 import com.example.docconneting.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    @Query("select p from Post p join fetch p.patient where p.id = :userId")
+    Optional<Post> findByIdWithUser(@Param(value = "userId") Long userId);
 }
