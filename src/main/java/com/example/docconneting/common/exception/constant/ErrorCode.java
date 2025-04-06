1f1db1cfa5e160a6c372292fb37780d1d8d800e6
suyeonkim <suyeonk56@gmail.com>
2025-04-06 21:39:50 +0900

    Feat : 비밀번호 수정, 의사 이미지 수정 기능 추가 ( #14 )

--- src/main/java/com/example/docconneting/common/exception/constant/ErrorCode.java
+++ src/main/java/com/example/docconneting/common/exception/constant/ErrorCode.java
@@ -9,15 +9,16 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // 회원 에러코드
-    //401
     // 403
     EMAIL_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다."),
+    PASSWORD_SAME_AS_OLD(HttpStatus.BAD_REQUEST,"기존 비밀번호와 동일한 비밀번호로 수정할 수 없습니다."),
 
 
     //로그인 에러 코드
     //401
-    USERROLE_NOT_FOUND(HttpStatus.BAD_REQUEST, "권한 이름을 잘못 입력하셨습니다."),
-    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    UNAUTHORIZED_USER(HttpStatus.UNAUTHORIZED, "의사만 접근 가능한 기능입니다."),
+    USERROLE_NOT_FOUND(HttpStatus.UNAUTHORIZED, "권한 이름을 잘못 입력하셨습니다."),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
 
     //404
     IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "이미지는 필수 입력 값입니다."),
