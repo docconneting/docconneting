92093fe1b99126dea7475789f56c871b46ead1bc
suyeonkim <suyeonk56@gmail.com>
2025-04-07 20:41:05 +0900

    Chore : 이메일 중복 확인 로직 추가, dto 이름 리네이밍, auth 패키지 이름 리네이밍

--- src/main/java/com/example/docconneting/domain/user/dto/request/UpdateImageRequest.java
+++ src/main/java/com/example/docconneting/domain/user/dto/request/UpdateImageRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
-public class UpdateImageRequestDto {
+public class UpdateImageRequest {
     @NotBlank(message = "신규 이미지는 필수사항입니다.")
     private String newImage;
 }
