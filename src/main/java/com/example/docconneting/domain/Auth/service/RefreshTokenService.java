58735ddc41c3811f80081974e3058e0be90750b8
suyeonkim <suyeonk56@gmail.com>
2025-04-07 19:33:42 +0900

    Refactor : 코드 리뷰 기반 수정 ( #14 )
    - @Transactional 및 readonly 추가

--- src/main/java/com/example/docconneting/domain/Auth/service/RefreshTokenService.java
+++ src/main/java/com/example/docconneting/domain/Auth/service/RefreshTokenService.java
@@ -4,6 +4,7 @@ import com.example.docconneting.common.config.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Duration;
 
@@ -15,16 +16,19 @@ public class RefreshTokenService {
     private final long REFRESH_TOKEN_EXPIRE_TIME = 14 * 24 * 60 * 60;
 
     //refreshToken 저장
+    @Transactional
     public void saveRefreshToken(Long userId, String refreshToken) {
         redisTemplate.opsForValue().set("refreshToken:" + userId, refreshToken, Duration.ofSeconds(REFRESH_TOKEN_EXPIRE_TIME));
     }
 
     //refreshToken 찾기
+    @Transactional(readOnly = true)
     public String getRefreshToken(Long userId) {
         return redisTemplate.opsForValue().get("refreshToken:" + userId);
     }
 
     //refreshToken 만료시간 확인
+    @Transactional(readOnly = true)
     public Long getRefreshTokenTTL(Long userId) {
         return redisTemplate.getExpire("refreshToken:" + userId);
     }
