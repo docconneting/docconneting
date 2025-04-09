f19449ee6d264a04e5663c5c4ce1f58b9b64a7b9
kyn1013 <yn112511@naver.com>
2025-04-09 23:28:08 +0900

    Feat : 유료 게시물을 등록한 경우, 댓글이 달린 경우, 채팅 결제가 완료된 경우 알람 기능 구현

--- src/main/java/com/example/docconneting/domain/alarm/repository/AlarmHistoriesBulkRepository.java
+++ src/main/java/com/example/docconneting/domain/alarm/repository/AlarmHistoriesBulkRepository.java
@@ -1,2 +1,33 @@
-package com.example.docconneting.domain.alarm.repository;public class AlarmHistoriesBulkRepository {
+package com.example.docconneting.domain.alarm.repository;
+
+import com.example.docconneting.domain.alarm.entity.AlarmHistories;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class AlarmHistoriesBulkRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public int[][] batchUpdate(final List<AlarmHistories> alarmHistoriesList) {
+        int [][] insertCount = jdbcTemplate.batchUpdate(
+                "INSERT INTO alarm_histories(content, to_id, alarm_type, created_at)" + "VALUES (?, ?, ?, ?)",
+                alarmHistoriesList,
+                100,
+                (PreparedStatement ps, AlarmHistories alarmHistories) -> {
+                    ps.setString(1, alarmHistories.getContent());
+                    ps.setString(2, String.valueOf(alarmHistories.getToId()));
+                    ps.setString(3, String.valueOf(alarmHistories.getAlarmType()));
+                    ps.setTimestamp(4, Timestamp.valueOf(LocalDateTime.now()));
+                });
+        return insertCount;
+    }
+
 }
