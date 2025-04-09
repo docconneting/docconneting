7cd724b2e8e2c9a1f4d0b7a0bd32f2f3ff5a1c01
kyn1013 <yn112511@naver.com>
2025-04-09 23:23:45 +0900

    Chore : 알람 타입 및 알람 히스토리 엔티티 수정 (#22)

---
+++ src/main/java/com/example/docconneting/domain/alarm/controller/AlarmController.java
@@ -0,0 +1,13 @@
+package com.example.docconneting.domain.alarm.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/alarm")
+@RequiredArgsConstructor
+public class AlarmController {
+
+
+}
