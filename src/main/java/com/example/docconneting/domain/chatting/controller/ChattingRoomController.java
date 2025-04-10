ce3267b40dd309ba1d21943c4b83552f3a56dcbe
tgg <qazplmtfgcv123@gmail.com>
2025-04-10 17:14:30 +0900

    Feat: 웹소켓 STOMP 구성 , 예외 처리, 채팅 기능 구현 (#35)

--- src/main/java/com/example/docconneting/domain/chatting/controller/ChattingRoomController.java
+++ src/main/java/com/example/docconneting/domain/chatting/controller/ChattingRoomController.java
@@ -18,40 +18,33 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class ChattingRoomController {
 
     private final ChattingRoomService chattingRoomService;
 
-    @MessageMapping("/chattingRooms/{chattingRoomId}")
-    @SendTo("/sub/chattingRooms/{chattingRoomId}")
-    public String sendMessage(@DestinationVariable Long chattingRoomId) throws Exception {
-        return HtmlUtils.htmlEscape(String.valueOf(chattingRoomId)) + "번 채팅 방";
-    }
-
-    @PostMapping("/api/v1/doctors/{doctorId}/chattingRooms")
+    @PostMapping("/doctors/{doctorId}/chattingRooms")
     public ResponseEntity<Response<ChattingRoomCreateResponse>> createdChattingRoom(@Auth AuthUser authUser, @PathVariable Long doctorId){
         return ResponseEntity
                 .ok()
                 .body(Response.of(chattingRoomService.createdChattingRoom(authUser, doctorId)));
     }
 
-    @GetMapping("/api/v1/chattingRooms/{chattingRoomId}")
+    @GetMapping("/chattingRooms/{chattingRoomId}")
     public ResponseEntity<Response<ChattingRoomSingleResponse>> findChattingRoomById(@Auth AuthUser authUser, @PathVariable Long chattingRoomId){
         return ResponseEntity
                 .ok()
                 .body(Response.of(chattingRoomService.findChattingRoomById(authUser, chattingRoomId)));
     }
 
-    @GetMapping("/api/v1/chattingRooms")
+    @GetMapping("/chattingRooms")
     public ResponseEntity<Response<List<ChattingRoomListResponse>>> findAllChattingRooms(@Auth AuthUser authUser, @PageableDefault(sort="createdAt", direction = Sort.Direction.DESC) Pageable pageable){
 
         PageResult<ChattingRoomListResponse> chattingRooms = chattingRoomService.findAllChattingRooms(authUser, pageable);
