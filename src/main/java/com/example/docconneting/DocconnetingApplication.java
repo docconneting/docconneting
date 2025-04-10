3263d042b7508d2d0b81b61f1a2ba015017ea21a
kyn1013 <yn112511@naver.com>
2025-04-10 22:21:28 +0900

    Fix : 의사들에게 게시물 등록시에 알람 가게 하는 기능 비동기로 돌아가도록 수정 (#22)

--- src/main/java/com/example/docconneting/DocconnetingApplication.java
+++ src/main/java/com/example/docconneting/DocconnetingApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableAsync
 @EnableJpaAuditing
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class DocconnetingApplication {
