91597069af57c2b5ddff6661c99c6385de278392
tgg <qazplmtfgcv123@gmail.com>
2025-04-12 17:50:22 +0900

    Feat: 채팅 예외 처리, 채팅 메시지 db에  저장 기능 구현 (#35)

--- src/main/java/com/example/docconneting/domain/chatting/controller/MessageController.java
+++ src/main/java/com/example/docconneting/domain/chatting/controller/MessageController.java
@@ -1,19 +1,32 @@
 package com.example.docconneting.domain.chatting.controller;
 
+import com.example.docconneting.domain.chatting.dto.request.MessageRequest;
+import com.example.docconneting.domain.chatting.dto.response.MessageResponse;
+import com.example.docconneting.domain.chatting.service.MessageService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class MessageController {
+
+    private final MessageService messageService;
+
     @MessageMapping("/chattingRooms/{chattingRoomId}")
     @SendTo("/sub/chattingRooms/{chattingRoomId}")
-    public String sendMessage(@DestinationVariable Long chattingRoomId, Message<?> message) throws Exception {
+    public MessageResponse sendMessage(@DestinationVariable Long chattingRoomId,
+                                       @Payload MessageRequest messageRequest,
+                                       Message<?> message) throws Exception {
+
         StompHeaderAccessor stompHeaderAccessor = StompHeaderAccessor.wrap(message);
         Long userId = (Long) stompHeaderAccessor.getSessionAttributes().get("userId");
-        return userId + "유저";
+
+        return messageService.createMessage(messageRequest, userId, chattingRoomId);
     }
 }
--- src/main/java/com/example/docconneting/domain/chatting/service/MessageService.java
+++ src/main/java/com/example/docconneting/domain/chatting/service/MessageService.java
@@ -1,6 +1,15 @@
 package com.example.docconneting.domain.chatting.service;
 
+import com.example.docconneting.common.exception.constant.ErrorCode;
+import com.example.docconneting.common.exception.object.ClientException;
+import com.example.docconneting.domain.chatting.dto.request.MessageRequest;
+import com.example.docconneting.domain.chatting.dto.response.MessageResponse;
+import com.example.docconneting.domain.chatting.entity.ChattingRoom;
+import com.example.docconneting.domain.chatting.entity.Message;
+import com.example.docconneting.domain.chatting.repository.ChattingRoomRepository;
 import com.example.docconneting.domain.chatting.repository.MessageRepository;
+import com.example.docconneting.domain.user.entity.User;
+import com.example.docconneting.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,10 +19,23 @@ import org.springframework.transaction.annotation.Transactional;
 public class MessageService {
 
     private final MessageRepository messageRepository;
+    private final UserRepository userRepository;
+    private final ChattingRoomRepository chattingRoomRepository;
 
     @Transactional
-    public void createMessage(){
+    public MessageResponse createMessage(MessageRequest messageRequest, Long userId, Long chattingRoomId){
 
+        // 채팅방에 들어올 삭제된 유저인지 여부를 확인 하므로 유저만 찾아오도록 함
+        User findUser = userRepository.findById(userId).orElseThrow(() -> new ClientException(ErrorCode.USER_NOT_FOUND));
+
+        // 채팅방에 들어올 때 비활성화 된 채팅방인지 여부를 확인 하므로 채팅방만 찾아오도록 함
+        ChattingRoom findChattingRoom = chattingRoomRepository.findById(chattingRoomId).orElseThrow(() -> new ClientException(ErrorCode.CHATTING_ROOM_NOT_FOUND));
+
+        Message message = Message.of(findUser, findChattingRoom, messageRequest.getContents());
+
+        Message savedMessage = messageRepository.save(message);
+
+        return MessageResponse.of(userId, findUser.getUsername(), savedMessage.getContents(), savedMessage.getCreatedAt());
     }
 
 }
