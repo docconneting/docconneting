715a17b6b570c191b5794bcaf2a5f3c489655d4f
wonchaebin <usan119@naver.com>
2025-04-15 14:12:38 +0900

    Feat: 스케줄러 추가로 만료시간 설정 (#39)

--- src/main/java/com/example/docconneting/domain/order/repository/OrderRepository.java
+++ src/main/java/com/example/docconneting/domain/order/repository/OrderRepository.java
@@ -3,6 +3,7 @@ package com.example.docconneting.domain.order.repository;
 import com.example.docconneting.domain.order.entity.Order;
 import com.example.docconneting.domain.order.enums.OrderStatus;
 import com.example.docconneting.domain.order.enums.OrderType;
+import com.example.docconneting.domain.payment.enums.PaymentStatus;
 import com.example.docconneting.domain.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +11,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
@@ -28,4 +31,14 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     Optional<Order> findLatestCompletedChatOrder(@Param("user") User user,
                                                  @Param("orderType") OrderType orderType,
                                                  @Param("orderStatus") OrderStatus orderStatus);
+
+    @Query("""
+    SELECT o FROM Order o
+    WHERE o.orderStatus = :orderStatus
+      AND o.paymentStatus = :paymentStatus
+      AND o.createdAt <= :threshold
+""")
+    List<Order> findAllByStatusBeforeTime(@Param("orderStatus") OrderStatus orderStatus,
+                                          @Param("paymentStatus") PaymentStatus paymentStatus,
+                                          @Param("threshold") LocalDateTime threshold);
 }
