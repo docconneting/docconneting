3373855ea95503963b29771ed6f41d19b0d0407b
suyeonkim <suyeonk56@gmail.com>
2025-05-02 20:58:45 +0900

    Refactor : elasticsearchConfig에서 로그인 삭제 ( #53 )

--- src/main/java/com/example/docconneting/common/config/ElasticsearchConfig.java
+++ src/main/java/com/example/docconneting/common/config/ElasticsearchConfig.java
@@ -1,45 +1,21 @@
 package com.example.docconneting.common.config;
 
-import org.jetbrains.annotations.NotNull;
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
 
 @Configuration
 public class ElasticsearchConfig extends ElasticsearchConfiguration {
 
-    private final Environment env;
-
-    @Value("${elastic.host}")
-    private String host;
-
-    @Value("${elastic.port}")
-    private String port;
-
-    @Value("${elastic.username:}")  // 빈 값 허용
-    private String username;
-
-    @Value("${elastic.password:}")  // 빈 값 허용
-    private String password;
-
-    public ElasticsearchConfig(Environment env) {
-        this.env = env;
-    }
-
     @Override
     public ClientConfiguration clientConfiguration() {
-        var builder = ClientConfiguration.builder()
-                .connectedTo(host + ":" + port)
-                .usingSsl(false);
-
-        if (env.acceptsProfiles("local")) {
-            builder.withBasicAuth(username, password);
-        }
-
-        return builder.build();
+        return ClientConfiguration.builder()
+                .connectedTo("localhost:9200")
+                .usingSsl(false)
+                .build();
     }
-
 }
