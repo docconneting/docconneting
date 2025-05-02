9073b64f98b37385f2c5d13b759ab7cb9187b514
tgg <qazplmtfgcv123@gmail.com>
2025-05-02 14:41:55 +0900

    Chore: ES 연결 ssl 사용하지 않음  (#78)

--- src/main/java/com/example/docconneting/common/config/ElasticsearchConfig.java
+++ src/main/java/com/example/docconneting/common/config/ElasticsearchConfig.java
@@ -1,6 +1,7 @@
 package com.example.docconneting.common.config;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
@@ -10,12 +11,18 @@ import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
 @Configuration
 public class ElasticsearchConfig extends ElasticsearchConfiguration {
 
+    @Value("${elastic.username}")
+    private String username;
+
+    @Value("${elastic.password}")
+    private String password;
+
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
                 .connectedTo("localhost:9200")
-                .usingSsl(true)
-                .withBasicAuth("elastic","429123")
+                .usingSsl(false)
+                .withBasicAuth(username, password)
                 .build();
     }
 }
