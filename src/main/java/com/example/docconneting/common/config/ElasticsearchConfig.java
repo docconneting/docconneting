e9c016e039603b05a6e4ff318e4d4c14dab5a5f3
suyeonkim <suyeonk56@gmail.com>
2025-05-02 19:44:33 +0900

    Chore : local일때만 엘라스틱 서치 로그인 하게 만들기 ( #53 )

--- src/main/java/com/example/docconneting/common/config/ElasticsearchConfig.java
+++ src/main/java/com/example/docconneting/common/config/ElasticsearchConfig.java
@@ -1,28 +1,45 @@
 package com.example.docconneting.common.config;
 
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
+
 
 @Configuration
 public class ElasticsearchConfig extends ElasticsearchConfiguration {
 
-    @Value("${elastic.username}")
+    private final Environment env;
+
+    @Value("${elastic.host}")
+    private String host;
+
+    @Value("${elastic.port}")
+    private String port;
+
+    @Value("${elastic.username:}")  // 빈 값 허용
     private String username;
 
-    @Value("${elastic.password}")
+    @Value("${elastic.password:}")  // 빈 값 허용
     private String password;
+
+    public ElasticsearchConfig(Environment env) {
+        this.env = env;
+    }
 
     @Override
     public ClientConfiguration clientConfiguration() {
-        return ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
-                .usingSsl(false)
-                .withBasicAuth(username, password)
-                .build();
+        var builder = ClientConfiguration.builder()
+                .connectedTo(host + ":" + port)
+                .usingSsl(false);
+
+        if (env.acceptsProfiles("local")) {
+            builder.withBasicAuth(username, password);
+        }
+
+        return builder.build();
     }
+
 }
