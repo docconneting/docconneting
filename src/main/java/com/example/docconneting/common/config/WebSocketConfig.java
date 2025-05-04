6147a7f522a85780124da3fcfb8641d0087a06d0
tgg <qazplmtfgcv123@gmail.com>
2025-05-04 19:53:10 +0900

    Chore: SimpBrokerMessageHandler 활성화 (#35)

--- src/main/java/com/example/docconneting/common/config/WebSocketConfig.java
+++ src/main/java/com/example/docconneting/common/config/WebSocketConfig.java
@@ -24,6 +24,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableSimpleBroker("/sub","/messages");
     }
 
     @Override
